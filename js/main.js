3fffd18c8e909cfc6582e9a8e2dbc6a6b07a24ec
tbo <patrick@thebeau.ca>
2025-05-04 15:55:20 -0400

    initial setup to start

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+//
+// Place any custom JS here
+//
+
+import * as bootstrap from 'bootstrap'
+
+document.querySelectorAll('[data-bs-toggle="popover"]')
+  .forEach(popover => {
+    new bootstrap.Popover(popover)
+  })
