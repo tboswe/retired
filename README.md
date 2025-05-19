63d5e4dff73db32f90af9b6946c06440dbe75d68
tbo <patrick@thebeau.ca>
2025-05-19 13:37:56 -0400

    updated what is visually generated for savings account selection

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # retired
 A tool to project earnings and retirement
+
+Scenarios
+single - not retired
+single - retired
+couple - not retired + not retired
+couple - not retired + retired
+couple - retired + retired
+
+Saving Accounts
+Cash
+Margin
+RSP
+SRSP
+TFSA
+LRSP
+LIRA
+
+Pension Accounts
+Government Pension
+Registered Pension
+LIF
+
--- js/main.js
+++ js/main.js
@@ -85,6 +85,10 @@ function justMeQuestions(){
           <input type="number" class="form-control" id="gross-income" min="0" max="10000000" value="50000" step="1000" onChange="saveUserData(this)" required>
         </div>
         <div class="col-lg-1">
+          <label for="income-raises-rate" class="form-label">Income Raise Rate</label>
+          <input type="number" class="form-control" id="raise-rate" min="0" max="100" value="5" step="1" onChange="saveUserData(this)" required>
+        </div>
+        <div class="col-lg-1">
           <input class="form-check-input" type="checkbox" id="eiCheckbox">
           <label class="form-check-label" for="eiCheckbox">EI</label>
           <input class="form-check-input" type="checkbox" id="cppCheckbox">
@@ -96,25 +100,259 @@ function justMeQuestions(){
           </button>
         </div>
       </div>
-      <h1 class="h4 mb-3">Rates</h1>
-      <div class="row align-items-end mb-3">
-        <div class="col-sm-2">
-          <label for="inflation-rate" class="form-label">Inflation Rate</label>
-          <input type="number" class="form-control" id="inflation-rate" min="0" max="100" value="3" step="1" onChange="saveUserData(this)" required>
+        <h1 class="h4 mb-3">Savings</h1>
+        <div class="row align-items-end mb-3">
+        <div class="col-lg-1">
+          <button type="button" id="add-button" class="btn btn-primary btn-sm" onClick="addSavingsAccount()">
+            <i class="bi bi-plus"></i> Add
+          </button>
         </div>
-        <div class="col-sm-2">
-          <label for="investment-rate" class="form-label ">Investment Return Rate</label>
-          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
-        </div>
-        <div class="col-sm-2">
-          <label for="income-raises-rate" class="form-label">Income Raise Rate</label>
-          <input type="number" class="form-control" id="raise-rate" min="0" max="100" value="3" step="5" onChange="saveUserData(this)" required>
-        </div>
+        <div class="col-lg-1">
+          <button type="button" id="savings-lock-button" class="btn btn-outline-secondary btn-sm" onClick="lockSavings()">
+            <i class="bi bi-plus"></i> Lock
+          </button>
+        </div>        
+
     </form>
   `;
 
   document.body.appendChild(justMeContainer);
 }
+
+function addSavingsAccount() {
+    const savingsRow = document.createElement('div');
+    savingsRow.className = 'row align-items-end mb-3';
+  
+    savingsRow.innerHTML = `
+      <div class="col-lg-2">
+        <label for="account-type" class="form-label">Account Type</label>
+        <select class="form-select" id="account-type" onChange="savingsAccountChange(this)" required>
+          <option value="cash">Cash</option>
+          <option value="margin">Margin</option>
+          <option value="rsp">RSP</option>
+          <option value="srsp">Spousal RSP</option>
+          <option value="mrsp">RSP Match</option>
+          <option value="tfsa">TFSA</option>
+          <option value="lrsp">LRSP</option>
+          <option value="lira">LIRA</option>
+          <option value="resp">RESP</option>
+        </select>
+      </div>
+        <div class="col-lg-2">
+          <label for="account-name" class="form-label">Account Name</label>
+          <input type="text" class="form-control" id="account-name" placeholder="Account Nickname" required>
+        </div>
+        <div class="col-lg-2">
+          <label for="account-balance" class="form-label">Balance</label>
+          <input type="number" class="form-control" id="account-balance" min="0" step="100" placeholder="Enter balance" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="investment-rate" class="form-label ">Return Rate</label>
+          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
+        </div> 
+      <div class="col-lg-1">
+        <button type="button" class="btn btn-danger btn-sm remove-row">
+          <i class="bi bi-trash"></i> Remove
+        </button>
+      </div>
+    `;
+  
+    // Append the new row to the Savings section
+    const savingsSection = document.querySelector('.row.align-items-end.mb-3:last-of-type');
+    savingsSection.parentNode.insertBefore(savingsRow, savingsSection.nextSibling);
+  
+    // Add event listener to the remove button
+    savingsRow.querySelector('.remove-row').addEventListener('click', () => {
+      savingsRow.remove();
+    });
+  }
+
+function savingsAccountChange(selectElement) {
+    const selectedValue = selectElement.value; // Get the selected dropdown value
+    const row = selectElement.closest('.row'); // Get the parent row of the dropdown
+  
+    // Clear the row's content except for the dropdown
+    row.innerHTML = `
+      <div class="col-lg-2">
+        <label for="account-type" class="form-label">Account Type</label>
+        <select class="form-select" id="account-type" onChange="savingsAccountChange(this)" required>
+          <option value="cash" ${selectedValue === 'cash' ? 'selected' : ''}>Cash</option>
+          <option value="margin" ${selectedValue === 'margin' ? 'selected' : ''}>Margin</option>
+          <option value="rsp" ${selectedValue === 'rsp' ? 'selected' : ''}>RSP</option>
+          <option value="srsp" ${selectedValue === 'srsp' ? 'selected' : ''}>Spousal RSP</option>
+          <option value="tfsa" ${selectedValue === 'tfsa' ? 'selected' : ''}>TFSA</option>
+          <option value="lrsp" ${selectedValue === 'lrsp' ? 'selected' : ''}>LRSP</option>
+          <option value="lira" ${selectedValue === 'lira' ? 'selected' : ''}>LIRA</option>
+          <option value="resp" ${selectedValue === 'resp' ? 'selected' : ''}>RESP</option>
+        </select>
+      </div>
+    `;
+  
+    // Add additional fields based on the selected value
+    if (selectedValue === 'cash' || selectedValue === 'margin') {
+      row.innerHTML += `
+        <div class="col-lg-2">
+          <label for="account-name" class="form-label">Account Name</label>
+          <input type="text" class="form-control" id="account-name" placeholder="Account Nickname" required>
+        </div>
+        <div class="col-lg-2">
+          <label for="account-balance" class="form-label">Balance</label>
+          <input type="number" class="form-control" id="account-balance" min="0" step="100" placeholder="Enter balance" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="investment-rate" class="form-label ">Return Rate</label>
+          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
+        </div> 
+
+      `;
+    } else if (selectedValue === 'rsp') {
+      row.innerHTML += `
+        <div class="col-lg-2">
+          <label for="account-name" class="form-label">Account Name</label>
+          <input type="text" class="form-control" id="account-name" placeholder="Account Nickname" required>
+        </div>
+        <div class="col-lg-2">
+          <label for="annuitant" class="form-label">Annuitant</label>
+          <input type="text" class="form-control" id="annuitant" placeholder="Who receives" required>
+        </div>
+        <div class="col-lg-2">      
+          <label for="account-balance" class="form-label">Balance</label>
+          <input type="number" class="form-control" id="account-balance" min="0" step="100" placeholder="Enter balance" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="investment-rate" class="form-label">Return Rate</label>
+          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
+        </div>
+      `;      
+    } else if (selectedValue === 'srsp'){
+      row.innerHTML += `
+      <div class="col-lg-2">
+        <label for="account-name" class="form-label">Account Name</label>
+        <input type="text" class="form-control" id="account-name" placeholder="Account Nickname" required>
+      </div>
+      <div class="col-lg-2">
+        <label for="contributor" class="form-label">Contributor</label>
+        <input type="text" class="form-control" id="contributor" placeholder="Who contributes" required>
+      </div>
+      <div class="col-lg-2">
+        <label for="annuitant" class="form-label">Annuitant</label>
+        <input type="text" class="form-control" id="annuitant" placeholder="Who receives" required>
+      </div>          
+      <div class="col-lg-2">
+        <label for="account-balance" class="form-label">Balance</label>
+        <input type="number" class="form-control" id="account-balance" min="0" step="100" placeholder="Enter balance" required>
+      </div>
+      <div class="col-lg-1">
+          <label for="investment-rate" class="form-label ">Return Rate</label>
+          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
+      </div>
+      `;
+    } else if (selectedValue === 'mrsp') {
+      row.innerHTML += `
+        <div class="col-lg-2">
+          <label for="account-name" class="form-label">Account Name</label>
+          <input type="text" class="form-control" id="account-name" placeholder="Account Nickname" required>
+        </div>
+        <div class="col-lg-2">
+          <label for="annuitant" class="form-label">Annuitant</label>
+          <input type="text" class="form-control" id="annuitant" placeholder="Who receives" required>
+        </div>   
+        <div class="col-lg-2">
+          <label for="account-balance" class="form-label">Balance</label>
+          <input type="number" class="form-control" id="account-balance" min="0" step="100" placeholder="Enter balance" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="match-rate" class="form-label ">Match Rate</label>
+          <input type="number" class="form-control" id="match-rate" min="0" max="100" value="4" step="1" onChange="saveUserData(this)" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="investment-rate" class="form-label ">Return Rate</label>
+          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
+        </div> 
+      `;
+    } else if (selectedValue === 'resp') {
+      row.innerHTML += `
+        <div class="col-lg-2">
+          <label for="account-name" class="form-label">Account Name</label>
+          <input type="text" class="form-control" id="account-name" placeholder="Account Nickname" required>
+        </div>      
+        <div class="col-lg-2">
+          <label for="account-balance" class="form-label">Balance</label>
+          <input type="number" class="form-control" id="account-balance" min="0" step="100" placeholder="Enter balance" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="investment-rate" class="form-label ">Return Rate</label>
+          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
+        </div>
+        <div class="col-lg-2">
+          <label for="yearly-contribution" class="form-label">Yearly Contribution</label>
+          <input type="number" class="form-control" id="yearly-contribution" min="0" step="100" placeholder="Enter yearly contribution" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="resp-contributions-end" class="form-label">Last Contribution Year</label>
+          <input type="number" class="form-control" id="resp-contributions-end" placeholder="Enter year of last contribution" required>
+        </div>
+      `;
+    } else if (selectedValue === 'tfsa') {
+      row.innerHTML += `
+        <div class="col-lg-2">
+          <label for="account-name" class="form-label">Account Name</label>
+          <input type="text" class="form-control" id="account-name" placeholder="Account Nickname" required>
+        </div>
+        <div class="col-lg-2">
+          <label for="annuitant" class="form-label">Annuitant</label>
+          <input type="text" class="form-control" id="annuitant" placeholder="Who receives" required>
+        </div>
+        <div class="col-lg-2">      
+          <label for="account-balance" class="form-label">Balance</label>
+          <input type="number" class="form-control" id="account-balance" min="0" step="100" placeholder="Enter balance" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="investment-rate" class="form-label">Return Rate</label>
+          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
+        </div>
+      `;   
+    } else if (selectedValue === 'lrsp' || selectedValue === 'lira'){ 
+      //similar to rsp but will link to an LIF and a year of LIF start
+      row.innerHTML += `
+        <div class="col-lg-2">
+          <label for="account-name" class="form-label">Account Name</label>
+          <input type="text" class="form-control" id="account-name" placeholder="Account Nickname" required>
+        </div>
+        <div class="col-lg-2">
+          <label for="annuitant" class="form-label">Annuitant</label>
+          <input type="text" class="form-control" id="annuitant" placeholder="Who receives" required>
+        </div>
+        <div class="col-lg-2">
+          <label for="linked-lif" class="form-label">Associated LIF</label>
+          <input type="text" class="form-control" id="linked-lif" placeholder="Linked LIF" required>
+        </div>
+        <div class="col-lg-2">      
+          <label for="account-balance" class="form-label">Balance</label>
+          <input type="number" class="form-control" id="account-balance" min="0" step="100" placeholder="Enter balance" required>
+        </div>
+        <div class="col-lg-1">
+          <label for="investment-rate" class="form-label">Return Rate</label>
+          <input type="number" class="form-control" id="investment-rate" min="0" max="100" value="7" step="1" onChange="saveUserData(this)" required>
+        </div>
+      `;       
+    }
+  
+    // Add the remove button back to the row
+    row.innerHTML += `
+      <div class="col-lg-1">
+        <button type="button" class="btn btn-danger btn-sm remove-row">
+          <i class="bi bi-trash"></i> Remove
+        </button>
+      </div>
+    `;
+  
+    // Add event listener to the remove button
+    row.querySelector('.remove-row').addEventListener('click', () => {
+      row.remove();
+    });
+  }
+
 
 function toggleLock() {
   const lockIcon = document.getElementById('lock-icon');
